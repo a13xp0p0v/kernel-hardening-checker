5c5aed3f39083733f2347e6cfba9c3399d8bd994
Anatoly Ivanov <bmth@me.com>
2018-07-29 01:32:25 +0300

    Removed long lines from Outputter

--- outputter.py
+++ outputter.py
@@ -2,7 +2,9 @@ class Outputter:
     @staticmethod
     def print_opt_checks(checklist):
         print('[+] Printing kernel hardening preferences...')
-        print('  {:<39}|{:^13}|{:^10}|{:^20}'.format('option name', 'desired val', 'decision', 'reason'))
+        print('  {:<39}|{:^13}|{:^10}|{:^20}'.format(
+            'option name', 'desired val', 'decision', 'reason')
+        )
         print('  ' + '=' * 88)
         for opt in checklist:
             print('  CONFIG_{:<32}|{:^13}|{:^10}|{:^20}'.format(
@@ -12,7 +14,9 @@ class Outputter:
 
     @staticmethod
     def print_check_results(checklist):
-        print('  {:<39}|{:^13}|{:^10}|{:^20}||{:^28}'.format('option name', 'desired val', 'decision', 'reason', 'check result'))
+        print('  {:<39}|{:^13}|{:^10}|{:^20}||{:^28}'.format(
+            'option name', 'desired val', 'decision', 'reason', 'check result')
+        )
         print('  ' + '=' * 117)
         for opt in checklist:
             print('  CONFIG_{:<32}|{:^13}|{:^10}|{:^20}||{:^28}'.format(
