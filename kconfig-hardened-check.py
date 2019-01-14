84951bf6804d25538e5127f381aaa64594140bec
Alexander Popov <alex.popov@linux.com>
2019-01-15 01:08:00 +0300

    Change 'decision' to 'defconfig' for hardening features enabled by default

--- kconfig-hardened-check.py
+++ kconfig-hardened-check.py
@@ -112,38 +112,38 @@ def construct_checklist():
     modules_not_set = OptCheck('MODULES',                'is not set', 'kspp', 'cut_attack_surface')
     devmem_not_set = OptCheck('DEVMEM',                  'is not set', 'kspp', 'cut_attack_surface') # refers to LOCK_DOWN_KERNEL
 
-    checklist.append(OptCheck('BUG',                         'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OptCheck('PAGE_TABLE_ISOLATION',        'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OptCheck('RETPOLINE',                   'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OptCheck('X86_64',                      'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OptCheck('X86_SMAP',                    'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OptCheck('X86_INTEL_UMIP',              'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OR(OptCheck('STRICT_KERNEL_RWX',        'y', 'ubuntu18', 'self_protection'), \
-                        OptCheck('DEBUG_RODATA',             'y', 'before_v4.11', 'self_protection')))
+    checklist.append(OptCheck('BUG',                         'y', 'defconfig', 'self_protection'))
+    checklist.append(OptCheck('PAGE_TABLE_ISOLATION',        'y', 'defconfig', 'self_protection'))
+    checklist.append(OptCheck('RETPOLINE',                   'y', 'defconfig', 'self_protection'))
+    checklist.append(OptCheck('X86_64',                      'y', 'defconfig', 'self_protection'))
+    checklist.append(OptCheck('X86_SMAP',                    'y', 'defconfig', 'self_protection'))
+    checklist.append(OptCheck('X86_INTEL_UMIP',              'y', 'defconfig', 'self_protection'))
+    checklist.append(OR(OptCheck('STRICT_KERNEL_RWX',        'y', 'defconfig', 'self_protection'), \
+                        OptCheck('DEBUG_RODATA',             'y', 'defconfig', 'self_protection'))) # before v4.11
     checklist.append(OptCheck('DEBUG_WX',                    'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OptCheck('RANDOMIZE_BASE',              'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OptCheck('RANDOMIZE_MEMORY',            'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OR(OptCheck('STACKPROTECTOR_STRONG',    'y', 'ubuntu18', 'self_protection'), \
-                        OptCheck('CC_STACKPROTECTOR_STRONG', 'y', 'ubuntu18', 'self_protection')))
-    checklist.append(OptCheck('VMAP_STACK',                  'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OptCheck('THREAD_INFO_IN_TASK',         'y', 'ubuntu18', 'self_protection'))
+    checklist.append(OptCheck('RANDOMIZE_BASE',              'y', 'defconfig', 'self_protection'))
+    checklist.append(OptCheck('RANDOMIZE_MEMORY',            'y', 'defconfig', 'self_protection'))
+    checklist.append(OR(OptCheck('STACKPROTECTOR_STRONG',    'y', 'defconfig', 'self_protection'), \
+                        OptCheck('CC_STACKPROTECTOR_STRONG', 'y', 'defconfig', 'self_protection')))
+    checklist.append(OptCheck('VMAP_STACK',                  'y', 'defconfig', 'self_protection'))
+    checklist.append(OptCheck('THREAD_INFO_IN_TASK',         'y', 'defconfig', 'self_protection'))
     checklist.append(OptCheck('SCHED_STACK_END_CHECK',       'y', 'ubuntu18', 'self_protection'))
-    checklist.append(OptCheck('SLUB_DEBUG',                  'y', 'ubuntu18', 'self_protection'))
+    checklist.append(OptCheck('SLUB_DEBUG',                  'y', 'defconfig', 'self_protection'))
     checklist.append(OptCheck('SLAB_FREELIST_HARDENED',      'y', 'ubuntu18', 'self_protection'))
     checklist.append(OptCheck('SLAB_FREELIST_RANDOM',        'y', 'ubuntu18', 'self_protection'))
     checklist.append(OptCheck('HARDENED_USERCOPY',           'y', 'ubuntu18', 'self_protection'))
     checklist.append(OptCheck('FORTIFY_SOURCE',              'y', 'ubuntu18', 'self_protection'))
     checklist.append(OptCheck('LOCK_DOWN_KERNEL',            'y', 'ubuntu18', 'self_protection')) # remember about LOCK_DOWN_MANDATORY
-    checklist.append(OR(OptCheck('STRICT_MODULE_RWX',        'y', 'ubuntu18', 'self_protection'), \
-                        OptCheck('DEBUG_SET_MODULE_RONX',    'y', 'before_v4.11', 'self_protection'), \
-                        modules_not_set))
+    checklist.append(OR(OptCheck('STRICT_MODULE_RWX',        'y', 'defconfig', 'self_protection'), \
+                        OptCheck('DEBUG_SET_MODULE_RONX',    'y', 'defconfig', 'self_protection'), \
+                        modules_not_set)) # DEBUG_SET_MODULE_RONX was before v4.11
     checklist.append(OR(OptCheck('MODULE_SIG',               'y', 'ubuntu18', 'self_protection'), \
                         modules_not_set))
     checklist.append(OR(OptCheck('MODULE_SIG_ALL',           'y', 'ubuntu18', 'self_protection'), \
                         modules_not_set))
     checklist.append(OR(OptCheck('MODULE_SIG_SHA512',        'y', 'ubuntu18', 'self_protection'), \
                         modules_not_set))
-    checklist.append(OptCheck('SYN_COOKIES',                 'y', 'ubuntu18', 'self_protection')) # another reason?
+    checklist.append(OptCheck('SYN_COOKIES',                 'y', 'defconfig', 'self_protection')) # another reason?
     checklist.append(OptCheck('DEFAULT_MMAP_MIN_ADDR',       '65536', 'ubuntu18', 'self_protection'))
 
     checklist.append(OptCheck('BUG_ON_DATA_CORRUPTION',           'y', 'kspp', 'self_protection'))
@@ -170,14 +170,15 @@ def construct_checklist():
     checklist.append(OptCheck('PAGE_POISONING_ZERO',              'is not set', 'my', 'self_protection'))
     checklist.append(OptCheck('SLAB_MERGE_DEFAULT',               'is not set', 'my', 'self_protection')) # slab_nomerge
 
-    checklist.append(OptCheck('SECURITY',                    'y', 'ubuntu18', 'security_policy'))
+    checklist.append(OptCheck('SECURITY',                    'y', 'defconfig', 'security_policy'))
     checklist.append(OptCheck('SECURITY_YAMA',               'y', 'ubuntu18', 'security_policy'))
     checklist.append(OptCheck('SECURITY_SELINUX_DISABLE',    'is not set', 'ubuntu18', 'security_policy'))
 
-    checklist.append(OptCheck('SECCOMP',              'y', 'ubuntu18', 'cut_attack_surface'))
-    checklist.append(OptCheck('SECCOMP_FILTER',       'y', 'ubuntu18', 'cut_attack_surface'))
-    checklist.append(OR(OptCheck('STRICT_DEVMEM',     'y', 'ubuntu18', 'cut_attack_surface'), \
+    checklist.append(OptCheck('SECCOMP',              'y', 'defconfig', 'cut_attack_surface'))
+    checklist.append(OptCheck('SECCOMP_FILTER',       'y', 'defconfig', 'cut_attack_surface'))
+    checklist.append(OR(OptCheck('STRICT_DEVMEM',     'y', 'defconfig', 'cut_attack_surface'), \
                         devmem_not_set)) # refers to LOCK_DOWN_KERNEL
+
     checklist.append(OptCheck('ACPI_CUSTOM_METHOD',   'is not set', 'ubuntu18', 'cut_attack_surface')) # refers to LOCK_DOWN_KERNEL
     checklist.append(OptCheck('COMPAT_BRK',           'is not set', 'ubuntu18', 'cut_attack_surface'))
     checklist.append(OptCheck('DEVKMEM',              'is not set', 'ubuntu18', 'cut_attack_surface'))
