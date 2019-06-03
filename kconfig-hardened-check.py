07687dfb97c20dbb80948cc059029165b3689df3
Alexander Popov <alex.popov@linux.com>
2019-06-03 19:59:25 +0300

    Improve printing of the results

--- kconfig-hardened-check.py
+++ kconfig-hardened-check.py
@@ -343,21 +343,21 @@ def construct_checklist(arch):
 
 def print_checklist(arch):
     print('[+] Printing kernel hardening preferences for {}...'.format(arch))
-    print('  {:<39}|{:^13}|{:^10}|{:^20}'.format(
+    print('{:^40}|{:^13}|{:^10}|{:^20}'.format(
         'option name', 'desired val', 'decision', 'reason'))
-    print('  ' + '=' * 86)
+    print('=' * 87)
     for opt in checklist:
-        print('  CONFIG_{:<32}|{:^13}|{:^10}|{:^20}'.format(
+        print('CONFIG_{:<33}|{:^13}|{:^10}|{:^20}'.format(
             opt.name, opt.expected, opt.decision, opt.reason))
     print()
 
 
 def print_check_results():
-    print('  {:<39}|{:^13}|{:^10}|{:^20}||{:^28}'.format(
+    print('{:^40}|{:^13}|{:^10}|{:^20}||{:^28}'.format(
         'option name', 'desired val', 'decision', 'reason', 'check result'))
-    print('  ' + '=' * 115)
+    print('=' * 116)
     for opt in checklist:
-        print('  CONFIG_{:<32}|{:^13}|{:^10}|{:^20}||{:^28}'.format(
+        print('CONFIG_{:<33}|{:^13}|{:^10}|{:^20}||{:^28}'.format(
             opt.name, opt.expected, opt.decision, opt.reason, opt.result))
     print()
 
