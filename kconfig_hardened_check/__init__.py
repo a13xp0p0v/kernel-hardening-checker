1556dfd237c2aa70337d57446c70dd0622b62ec3
Alexander Popov <alex.popov@linux.com>
2022-08-13 22:50:00 +0300

    Add the comment about vm.mmap_min_addr sysctl (for future reference)

--- kconfig_hardened_check/__init__.py
+++ kconfig_hardened_check/__init__.py
@@ -56,6 +56,7 @@
 #    vm.unprivileged_userfaultfd=0
 #        (at first, it disabled unprivileged userfaultfd,
 #         and since v5.11 it enables unprivileged userfaultfd for user-mode only)
+#    vm.mmap_min_addr has a good value
 #    dev.tty.ldisc_autoload=0
 #    fs.protected_symlinks=1
 #    fs.protected_hardlinks=1
