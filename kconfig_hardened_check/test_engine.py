7d62a7fba7370b6bdf1f8dbe7377f67436596e85
Alexander Popov <alex.popov@linux.com>
2023-03-24 23:10:35 +0300

    Add the first unit-test draft

--- kconfig_hardened_check/test_engine.py
+++ kconfig_hardened_check/test_engine.py
@@ -11,11 +11,49 @@ This module performs unit-testing of the kconfig-hardened-check engine.
 """
 
 import unittest
-# from .engine import populate_with_data, perform_checks
+from collections import OrderedDict
+import json
+from .engine import KconfigCheck, CmdlineCheck, populate_with_data, perform_checks
 
 class TestEngine(unittest.TestCase):
-    def test_upper(self):
+    def test_1(self):
+        # add checks to the checklist
+        config_checklist = []
+        config_checklist += [KconfigCheck('reason_1', 'decision_1', 'KCONFIG_NAME', 'expected_1')]
+        config_checklist += [CmdlineCheck('reason_2', 'decision_2', 'cmdline_name', 'expected_2')]
+
+        # populate the checklist with the parsed kconfig data
+        parsed_kconfig_options = OrderedDict()
+        parsed_kconfig_options['CONFIG_KCONFIG_NAME'] = 'UNexpected_1'
+        populate_with_data(config_checklist, parsed_kconfig_options, 'kconfig')
+
+        # populate the checklist with the parsed cmdline data
+        parsed_cmdline_options = OrderedDict()
+        parsed_cmdline_options['cmdline_name'] = 'expected_2'
+        populate_with_data(config_checklist, parsed_cmdline_options, 'cmdline')
+
+        # populate the checklist with the kernel version data
+        kernel_version = (42, 43)
+        populate_with_data(config_checklist, kernel_version, 'version')
+
+        # now everything is ready, perform the checks
+        perform_checks(config_checklist)
+
+        # print the results in json
+        output = []
+        print('JSON:')
+        for opt in config_checklist:
+            output.append(opt.json_dump(True))
+        print(json.dumps(output))
+
+        # print the results
+        print('TABLE:')
+        for opt in config_checklist:
+            opt.table_print(None, True)
+            print()
+        print()
+
         self.assertEqual('foo'.upper(), 'FOO')
 
-    def test_isupper(self):
+    def test_2(self):
         self.assertTrue('FOO'.isupper())
