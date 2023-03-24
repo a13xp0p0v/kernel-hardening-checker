453618bcfdeec4521b98dff9e9c669aa95d9c1a8
Alexander Popov <alex.popov@linux.com>
2023-03-24 23:10:57 +0300

    Create the run_engine() helper

--- kconfig_hardened_check/test_engine.py
+++ kconfig_hardened_check/test_engine.py
@@ -16,43 +16,50 @@ import json
 from .engine import KconfigCheck, CmdlineCheck, populate_with_data, perform_checks
 
 class TestEngine(unittest.TestCase):
+    def run_engine(self, checklist, parsed_kconfig_options, parsed_cmdline_options, kernel_version):
+        # populate the checklist with data
+        populate_with_data(checklist, parsed_kconfig_options, 'kconfig')
+        populate_with_data(checklist, parsed_cmdline_options, 'cmdline')
+        populate_with_data(checklist, kernel_version, 'version')
+
+        # now everything is ready, perform the checks
+        perform_checks(checklist)
+
+        # print the results in JSON
+        output = []
+        print('JSON:')
+        for opt in checklist:
+            output.append(opt.json_dump(True)) # with_results
+        print(json.dumps(output))
+
+        # print the table with the results
+        print('TABLE:')
+        for opt in checklist:
+            opt.table_print(None, True) # default mode, with_results
+            print()
+        print()
+
     def test_1(self):
-        # add checks to the checklist
+        # 1. prepare the checklist
         config_checklist = []
         config_checklist += [KconfigCheck('reason_1', 'decision_1', 'KCONFIG_NAME', 'expected_1')]
         config_checklist += [CmdlineCheck('reason_2', 'decision_2', 'cmdline_name', 'expected_2')]
 
-        # populate the checklist with the parsed kconfig data
+        # 2. prepare the parsed kconfig options
         parsed_kconfig_options = OrderedDict()
         parsed_kconfig_options['CONFIG_KCONFIG_NAME'] = 'UNexpected_1'
-        populate_with_data(config_checklist, parsed_kconfig_options, 'kconfig')
 
-        # populate the checklist with the parsed cmdline data
+        # 3. prepare the parsed cmdline options
         parsed_cmdline_options = OrderedDict()
         parsed_cmdline_options['cmdline_name'] = 'expected_2'
-        populate_with_data(config_checklist, parsed_cmdline_options, 'cmdline')
 
-        # populate the checklist with the kernel version data
+        # 4. prepare the kernel version
         kernel_version = (42, 43)
-        populate_with_data(config_checklist, kernel_version, 'version')
 
-        # now everything is ready, perform the checks
-        perform_checks(config_checklist)
+        # 5. run the engine
+        self.run_engine(config_checklist, parsed_kconfig_options, parsed_cmdline_options, kernel_version)
 
-        # print the results in json
-        output = []
-        print('JSON:')
-        for opt in config_checklist:
-            output.append(opt.json_dump(True))
-        print(json.dumps(output))
-
-        # print the results
-        print('TABLE:')
-        for opt in config_checklist:
-            opt.table_print(None, True)
-            print()
-        print()
-
+        # 6. check that the results are correct
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_2(self):
