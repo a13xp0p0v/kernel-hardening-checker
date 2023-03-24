850426de65e891be9d87fc90f11c56dbb1545252
Alexander Popov <alex.popov@linux.com>
2023-03-24 23:18:32 +0300

    test_engine: reorganize single unit-tests

--- kconfig_hardened_check/test_engine.py
+++ kconfig_hardened_check/test_engine.py
@@ -76,32 +76,17 @@ class TestEngine(unittest.TestCase):
         print(json.dumps(result))
         print()
 
-    def test_kconfig_ok(self):
+    def test_single_kconfig(self):
         # 1. prepare the checklist
         config_checklist = []
         config_checklist += [KconfigCheck('reason_1', 'decision_1', 'NAME_1', 'expected_1')]
+        config_checklist += [KconfigCheck('reason_2', 'decision_2', 'NAME_2', 'expected_2')]
+        config_checklist += [KconfigCheck('reason_3', 'decision_3', 'NAME_3', 'expected_3')]
+        config_checklist += [KconfigCheck('reason_4', 'decision_4', 'NAME_4', 'is not set')]
 
         # 2. prepare the parsed kconfig options
         parsed_kconfig_options = OrderedDict()
         parsed_kconfig_options['CONFIG_NAME_1'] = 'expected_1'
-
-        # 3. run the engine
-        result = []
-        self.run_engine(config_checklist, parsed_kconfig_options, None, None, result)
-
-        # 4. check that the results are correct
-        self.assertEqual(
-                result,
-                [["CONFIG_NAME_1", "kconfig", "expected_1", "decision_1", "reason_1", "OK"]]
-        )
-
-    def test_kconfig_fail(self):
-        # 1. prepare the checklist
-        config_checklist = []
-        config_checklist += [KconfigCheck('reason_2', 'decision_2', 'NAME_2', 'expected_2')]
-
-        # 2. prepare the parsed kconfig options
-        parsed_kconfig_options = OrderedDict()
         parsed_kconfig_options['CONFIG_NAME_2'] = 'UNexpected_2'
 
         # 3. run the engine
@@ -111,17 +96,25 @@ class TestEngine(unittest.TestCase):
         # 4. check that the results are correct
         self.assertEqual(
                 result,
-                [["CONFIG_NAME_2", "kconfig", "expected_2", "decision_2", "reason_2", "FAIL: \"UNexpected_2\""]]
+                [["CONFIG_NAME_1", "kconfig", "expected_1", "decision_1", "reason_1", "OK"],
+                 ["CONFIG_NAME_2", "kconfig", "expected_2", "decision_2", "reason_2", "FAIL: \"UNexpected_2\""],
+                 ["CONFIG_NAME_3", "kconfig", "expected_3", "decision_3", "reason_3", "FAIL: is not found"],
+                 ["CONFIG_NAME_4", "kconfig", "is not set", "decision_4", "reason_4", "OK: is not found"]]
         )
 
-    def test_cmdline_ok(self):
+    def test_single_cmdline(self):
         # 1. prepare the checklist
         config_checklist = []
+        config_checklist += [CmdlineCheck('reason_1', 'decision_1', 'name_1', 'expected_1')]
+        config_checklist += [CmdlineCheck('reason_2', 'decision_2', 'name_2', 'expected_2')]
         config_checklist += [CmdlineCheck('reason_3', 'decision_3', 'name_3', 'expected_3')]
+        config_checklist += [CmdlineCheck('reason_4', 'decision_4', 'name_4', 'is not set')]
+
 
         # 2. prepare the parsed cmdline options
         parsed_cmdline_options = OrderedDict()
-        parsed_cmdline_options['name_3'] = 'expected_3'
+        parsed_cmdline_options['name_1'] = 'expected_1'
+        parsed_cmdline_options['name_2'] = 'UNexpected_2'
 
         # 3. run the engine
         result = []
@@ -130,41 +123,8 @@ class TestEngine(unittest.TestCase):
         # 4. check that the results are correct
         self.assertEqual(
                 result,
-                [["name_3", "cmdline", "expected_3", "decision_3", "reason_3", "OK"]]
-        )
-
-    def test_cmdline_fail(self):
-        # 1. prepare the checklist
-        config_checklist = []
-        config_checklist += [CmdlineCheck('reason_4', 'decision_4', 'name_4', 'expected_4')]
-
-        # 2. prepare the parsed cmdline options
-        parsed_cmdline_options = OrderedDict()
-        parsed_cmdline_options['name_4'] = 'UNexpected_4'
-
-        # 3. run the engine
-        result = []
-        self.run_engine(config_checklist, None, parsed_cmdline_options, None, result)
-
-        # 4. check that the results are correct
-        self.assertEqual(
-                result,
-                [["name_4", "cmdline", "expected_4", "decision_4", "reason_4", "FAIL: \"UNexpected_4\""]]
-        )
-
-    def test_kconfig_not_found(self):
-        # 1. prepare the checklist
-        config_checklist = []
-        config_checklist += [KconfigCheck('reason_5', 'decision_5', 'NAME_5', 'expected_5')]
-        config_checklist += [KconfigCheck('reason_6', 'decision_6', 'NAME_6', 'is not set')]
-
-        # 2. run the engine
-        result = []
-        self.run_engine(config_checklist, None, None, None, result)
-
-        # 3. check that the results are correct
-        self.assertEqual(
-                result,
-                [["CONFIG_NAME_5", "kconfig", "expected_5", "decision_5", "reason_5", "FAIL: is not found"],
-                 ["CONFIG_NAME_6", "kconfig", "is not set", "decision_6", "reason_6", "OK: is not found"]]
+                [["name_1", "cmdline", "expected_1", "decision_1", "reason_1", "OK"],
+                 ["name_2", "cmdline", "expected_2", "decision_2", "reason_2", "FAIL: \"UNexpected_2\""],
+                 ["name_3", "cmdline", "expected_3", "decision_3", "reason_3", "FAIL: is not found"],
+                 ["name_4", "cmdline", "is not set", "decision_4", "reason_4", "OK: is not found"]]
         )
