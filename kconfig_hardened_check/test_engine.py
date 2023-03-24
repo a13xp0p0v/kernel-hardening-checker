dd45244eae35fbbdeaf7783ac2f4700771b0504a
Alexander Popov <alex.popov@linux.com>
2023-03-24 23:11:49 +0300

    Fix pylint warnings: add class docstring

--- kconfig_hardened_check/test_engine.py
+++ kconfig_hardened_check/test_engine.py
@@ -15,7 +15,37 @@ from collections import OrderedDict
 import json
 from .engine import KconfigCheck, CmdlineCheck, populate_with_data, perform_checks
 
+
 class TestEngine(unittest.TestCase):
+    """
+    Example test scenario:
+
+        # 1. prepare the checklist
+        config_checklist = []
+        config_checklist += [KconfigCheck('reason_1', 'decision_1', 'KCONFIG_NAME', 'expected_1')]
+        config_checklist += [CmdlineCheck('reason_2', 'decision_2', 'cmdline_name', 'expected_2')]
+
+        # 2. prepare the parsed kconfig options
+        parsed_kconfig_options = OrderedDict()
+        parsed_kconfig_options['CONFIG_KCONFIG_NAME'] = 'UNexpected_1'
+
+        # 3. prepare the parsed cmdline options
+        parsed_cmdline_options = OrderedDict()
+        parsed_cmdline_options['cmdline_name'] = 'expected_2'
+
+        # 4. prepare the kernel version
+        kernel_version = (42, 43)
+
+        # 5. run the engine
+        result = []
+        self.run_engine(config_checklist,
+                        parsed_kconfig_options, parsed_cmdline_options, kernel_version,
+                        result)
+
+        # 6. check that the results are correct
+        # self.assertEqual(...
+    """
+
     @staticmethod
     def run_engine(checklist, parsed_kconfig_options, parsed_cmdline_options, kernel_version):
         # populate the checklist with data
