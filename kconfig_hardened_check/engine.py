7d95d59bad5f740d524f9f988468bd182305c1b8
Frak <frak.alnuaimy@technicolor.com>
2023-09-10 13:40:59 -0400

    cleanup spaces

--- kconfig_hardened_check/engine.py
+++ kconfig_hardened_check/engine.py
@@ -91,7 +91,7 @@ class OptCheck:
     def table_print(self, _mode, with_results):
         print(f'{self.name:<40}|{self.type:^7}|{self.expected:^12}|{self.decision:^10}|{self.reason:^18}', end='')
         if with_results:
-            print(f'| {colorize_result(self.result)}', end='') 
+            print(f'| {colorize_result(self.result)}', end='')
 
     def json_dump(self, with_results):
         dump = [self.name, self.type, self.expected, self.decision, self.reason]
@@ -150,7 +150,7 @@ class VersionCheck:
         ver_req = f'kernel version >= {self.ver_expected[0]}.{self.ver_expected[1]}'
         print(f'{ver_req:<91}', end='')
         if with_results:
-            print(f'| {colorize_result(self.result)}', end='') 
+            print(f'| {colorize_result(self.result)}', end='')
 
 
 class ComplexOptCheck:
@@ -180,7 +180,7 @@ class ComplexOptCheck:
         if mode == 'verbose':
             print(f'    {"<<< " + self.__class__.__name__ + " >>>":87}', end='')
             if with_results:
-                print(f'| {colorize_result(self.result)}', end='') 
+                print(f'| {colorize_result(self.result)}', end='')
             for o in self.opts:
                 print()
                 o.table_print(mode, with_results)
@@ -188,7 +188,7 @@ class ComplexOptCheck:
             o = self.opts[0]
             o.table_print(mode, False)
             if with_results:
-                 print(f'| {colorize_result(self.result)}', end='') 
+                 print(f'| {colorize_result(self.result)}', end=')
 
     def json_dump(self, with_results):
         dump = self.opts[0].json_dump(False)
