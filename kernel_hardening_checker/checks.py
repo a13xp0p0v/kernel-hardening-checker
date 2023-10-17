b69b652596d1d1aba5f6716418066bf39110565e
Alexander Popov <alex.popov@linux.com>
2023-10-17 19:19:06 +0300

    Add the 'fs.protected_fifos' check

--- kernel_hardening_checker/checks.py
+++ kernel_hardening_checker/checks.py
@@ -580,7 +580,6 @@ def normalize_cmdline_options(option, value):
 # TODO: draft of security hardening sysctls:
 #    what about bpf_jit_enable?
 #    vm.mmap_min_addr has a good value
-#    fs.protected_fifos=2
 #    fs.protected_regular=2
 #    fs.suid_dumpable=0
 #    kernel.modules_disabled=1
@@ -616,3 +615,4 @@ def add_sysctl_checks(l, arch):
 
     l += [SysctlCheck('harden_userspace', 'kspp', 'fs.protected_symlinks', '1')]
     l += [SysctlCheck('harden_userspace', 'kspp', 'fs.protected_hardlinks', '1')]
+    l += [SysctlCheck('harden_userspace', 'kspp', 'fs.protected_fifos', '2')]
