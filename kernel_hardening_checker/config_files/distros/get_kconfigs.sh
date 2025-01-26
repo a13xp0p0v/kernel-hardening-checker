b9cb7b08a99b630a85442e5281a5a42ffb6e2bbc
bcoles <bcoles@gmail.com>
2025-01-26 14:40:08 +1100

    Add RISC-V kernel config distro files

--- kernel_hardening_checker/config_files/distros/get_kconfigs.sh
+++ kernel_hardening_checker/config_files/distros/get_kconfigs.sh
@@ -33,6 +33,7 @@ wget -O Arch_hardened_x86_64.config https://gitlab.archlinux.org/archlinux/packa
 wget -O Azure_Linux_x86_64.config https://raw.githubusercontent.com/microsoft/azurelinux/refs/heads/3.0/SPECS/kernel/config
 wget -O OpenSUSE_x86_64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/master/config/x86_64/default
 wget -O OpenSUSE_aarch64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/master/config/arm64/default
+wget -O OpenSUSE_riscv64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/master/config/riscv64/default
 wget -O SLE-15-SP7_x86_64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/SLE15-SP7/config/x86_64/default
 wget -O SLE-15-SP7_aarch64.config https://raw.githubusercontent.com/openSUSE/kernel-source/refs/heads/SLE15-SP7/config/arm64/default
 
