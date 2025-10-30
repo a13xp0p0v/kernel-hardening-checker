6b73f7c4eb8070cb277705df7641c30ec918f992
Alexander Popov <alex.popov@linux.com>
2025-10-30 21:48:28 +0300

    Update distro configs

--- kernel_hardening_checker/config_files/distros/get_kconfigs.sh
+++ kernel_hardening_checker/config_files/distros/get_kconfigs.sh
@@ -6,23 +6,20 @@ set -e
 oracle_git_url="https://raw.githubusercontent.com/oracle/kconfigs/refs/heads/gh-pages/out/"
 
 kconfigs_from_oracle=(
-    "Alma Linux 9 aarch64"
-    "Alma Linux 9 x86_64"
+    "Alma Linux 10 aarch64"
+    "Alma Linux 10 x86_64"
     "Android 15 (6.6) aarch64"
     "Arch x86_64"
     "CentOS 10 Stream aarch64"
     "CentOS 10 Stream x86_64"
-    "Debian 10 Buster aarch64" # old, for history
-    "Debian 10 Buster x86_64" # old, for history
     "Debian 13 Trixie aarch64"
     "Debian 13 Trixie x86_64"
     "Fedora 42 Updates aarch64"
     "Fedora 42 Updates x86_64"
+    "Fedora Rawhide x86_64"
     "Fedora Asahi Remix Rawhide aarch64"
     "Oracle Linux 10 (UEK-NEXT) aarch64"
     "Oracle Linux 10 (UEK-NEXT) x86_64"
-    "Ubuntu 20.04 LTS Focal aarch64" # old, for history
-    "Ubuntu 20.04 LTS Focal x86_64" # old, for history
     "Ubuntu 25.04 Oracular aarch64"
     "Ubuntu 25.04 Plucky x86_64"
 )
