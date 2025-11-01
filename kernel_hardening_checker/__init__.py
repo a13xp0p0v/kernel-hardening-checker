afc376f2a935994793343cfeb05953583cc30191
Alexander Popov <alex.popov@linux.com>
2025-11-02 02:18:23 +0300

    Ready for the release 0.6.17.1

--- kernel_hardening_checker/__init__.py
+++ kernel_hardening_checker/__init__.py
@@ -28,7 +28,7 @@ from .engine import print_unknown_options, populate_with_data, perform_checks, o
 
 # The kernel-hardening-checker version format:
 #   'major.linux_kernel_version.minor'
-__version__ = '0.6.10.2'
+__version__ = '0.6.17.1'
 
 SUPPORTED_ARCHS = ['X86_64', 'X86_32', 'ARM64', 'ARM', 'RISCV']
 
