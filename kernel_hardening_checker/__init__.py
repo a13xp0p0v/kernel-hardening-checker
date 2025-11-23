c33e398855ee7ff574e9e1a9a92779e5aaeda398
Alexander Popov <alex.popov@linux.com>
2025-11-24 01:08:53 +0300

    By the way, use `re.match()` instead of `re.search()` to avoid mistakes
    Here we should check the pattern from the beginning of a string.

--- kernel_hardening_checker/__init__.py
+++ kernel_hardening_checker/__init__.py
@@ -93,7 +93,7 @@ def detect_arch_by_kconfig(fname: str) -> tuple[StrOrNone, str]:
 
     with _open(fname) as f:
         for line in f.readlines():
-            if m := re.search("CONFIG_([A-Z0-9_]+)=y$", line):
+            if m := re.match("CONFIG_([A-Z0-9_]+)=y$", line):
                 option = m.group(1)
                 if option not in SUPPORTED_ARCHS:
                     continue
@@ -121,7 +121,7 @@ def detect_arch_by_sysctl(fname: str) -> tuple[StrOrNone, str]:
                 value = line.split('=', 1)[1].strip()
                 for arch, pattern in arch_mapping.items():
                     assert(arch in SUPPORTED_ARCHS), 'invalid arch mapping in sysctl'
-                    if re.search(pattern, value):
+                    if re.match(pattern, value):
                         return arch, value
                 return None, f'{value} is an unsupported arch'
         return None, 'failed to detect architecture in sysctl'
