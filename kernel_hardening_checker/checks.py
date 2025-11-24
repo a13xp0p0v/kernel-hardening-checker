820e4036b8e53b809aaed6cd20174791bbcc1b48
Willenst <willenst@gmail.com>
2025-11-24 13:48:34 +0200

    Move apparmor_restrict_unprivileged_unconfined check in a propper place

--- kernel_hardening_checker/checks.py
+++ kernel_hardening_checker/checks.py
@@ -900,6 +900,12 @@ def add_sysctl_checks(l: list[ChecklistObjType], arch: StrOrNone) -> None:
     l += [SysctlCheck('self_protection', 'a13xp0p0v', 'kernel.oops_limit', '100')]
     l += [SysctlCheck('self_protection', 'a13xp0p0v', 'kernel.warn_limit', '100')]
 
+    # 'security_policy', 'ubuntu'
+    l += [SysctlCheck('security_policy', 'ubuntu', 'kernel.apparmor_restrict_unprivileged_unconfined', '1')]
+          # Prevents an attacker from changing their own process's AppArmor profiles
+          # For example, it can be used to bypass user namespaces hardenings in Ubuntu
+          # https://u1f383.github.io/linux/2025/06/26/the-journey-of-bypassing-ubuntus-unprivileged-namespace-restriction.html
+
     # 'cut_attack_surface', 'kspp'
     l += [SysctlCheck('cut_attack_surface', 'kspp', 'kernel.dmesg_restrict', '1')]
     l += [SysctlCheck('cut_attack_surface', 'kspp', 'kernel.perf_event_paranoid', '3')]
@@ -979,12 +985,6 @@ def add_sysctl_checks(l: list[ChecklistObjType], arch: StrOrNone) -> None:
     #  CAUTION: messages about packets with un-routable source addresses may clog up the kernel log
     #   l += [SysctlCheck('network_security', 'cis', 'net.ipv4.conf.all.log_martians', '1')]
     #   l += [SysctlCheck('network_security', 'cis', 'net.ipv4.conf.default.log_martians', '1')]
-
-    # 'security_policy', 'ubuntu'
-    l += [SysctlCheck('security_policy', 'ubuntu', 'kernel.apparmor_restrict_unprivileged_unconfined', '1')]
-          # Prevents an attacker from changing their own process's AppArmor profiles
-          # For example, it can be used to bypass user namespaces hardenings in Ubuntu
-          # https://u1f383.github.io/linux/2025/06/26/the-journey-of-bypassing-ubuntus-unprivileged-namespace-restriction.html
 
     # 'harden_userspace', 'kspp'
     l += [SysctlCheck('harden_userspace', 'kspp', 'fs.protected_symlinks', '1')]
